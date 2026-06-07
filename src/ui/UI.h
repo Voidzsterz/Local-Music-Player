d7079b7ca6f991ba8c22d823b9bd7a100c4fb32a
Voidzsterz <fabian.isib@gmail.com>
2026-06-07 09:35:06 +0100

    Updated UI.h

---
+++ src/ui/UI.h
@@ -0,0 +1,15 @@
+#pragma once
+
+class UI
+{
+public:
+    void render();
+private:
+    // m_activeTab will decide what to render inside the "content" page
+    enum class ActiveTab { Home, Songs, Library, Directories, Settings, };
+    ActiveTab m_activeTab = ActiveTab::Home;
+
+    void renderSidebar();
+    void renderContents();
+    void renderPlaying();
+};
