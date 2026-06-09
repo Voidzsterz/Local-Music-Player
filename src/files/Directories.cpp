b69627ce4d80f54b8c0ec5e83ed0cb52bcf9a55b
Voidzsterz <fabian.isib@gmail.com>
2026-06-09 19:57:33 +0100

    Added 3 functions to Directories.cpp (And minor naming changes)
    Added 3 out of 5 planned functioons for Directories.cpp:
    addDirectory (formerly allocateDirectory)
    removeDirectory (formerly deallocateDirectory)
    clearDirectories
    
    Name changes were stated above, planning to create scanDirectories, and
    promptDirectorySelect next

--- src/files/Directories.cpp
+++ src/files/Directories.cpp
@@ -1,29 +1,48 @@
 #include "filesystem"
 #include <vector>
+#include <algorithm>
+#include <iostream>
 
 #include "Directories.h"
 
 void Directories::promptDirectorySelect()
 {
-
+    // This will be added later, for now "test" directories will be used for debugging (or directly inputting directories via a string maybe)
 }
 
-void Directories::allocateDirectory()
+void Directories::addDirectory(const std::string& path)
 {
-
+    std::cout << "Attempting addition of directory: " << path.c_str() << std::endl;
+    if (std::filesystem::exists(path) && std::filesystem::is_directory(path))
+    {
+        if (std::find(m_directories.begin(), m_directories.end(), path) == m_directories.end())
+        {
+            m_directories.push_back(path);
+            std::cout << "Successfully added directory: " << path.c_str() << std::endl;
+        }
+    }
 }
 
-void Directories::deallocateDirectory()
+void Directories::removeDirectory(const std::string& path)
 {
-
+    // We can simply use ".erase" for this, no need to loop through everything like in addDirectory
+    // If erase doesn't find anything, nothing happens, it works out whether the directory exists or not
+    std::cout << "Attempting removal of directory: " << path.c_str() << std::endl;
+    m_directories.erase
+    (
+        std::remove(m_directories.begin(), m_directories.end(), path), m_directories.end()
+    );
 }
 
 void Directories::clearDirectories()
 {
-
+    std::cout << "Clearing all directories..." << std::endl;
+    m_directories.clear();
 }
 
 void Directories::scanDirectories()
 {
-    
+    // not sure how im gonna make this work
+    // TODO: MAKE THIS WORK
+    std::cout << "(WIP) This is a test print for scanDirectories" << std::endl;
 }
--- src/files/Directories.h
+++ src/files/Directories.h
@@ -6,12 +6,15 @@
 class Directories
 {
 public:
+    using Path = std::filesystem::path; // Creates a "type alias" for std::filesystem::path (nothing changes, just easier for me to read)
+    const std::vector<Path>& view() const { return m_directories; } // Essentially returns a read-only REFERENCE to the directories array
+
     void promptDirectorySelect(); // Long/confusing name, might change
-    void allocateDirectory();
-    void deallocateDirectory();
+    void addDirectory(const std::string& path);
+    void removeDirectory(const std::string& path);
     void clearDirectories();
     void scanDirectories();
 private:
     // Vector array containing all directories
-    std::vector<std::filesystem::path> m_directories;
-};
+    std::vector<Path> m_directories;
+}; 
