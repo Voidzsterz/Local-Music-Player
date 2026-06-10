b23d0e4ecb69b4eeddbfeb10fbd0abd6ea9f7ac4
Voidzsterz <fabian.isib@gmail.com>
2026-06-10 16:57:18 +0100

    Adding functionality for multi-selecting and deleting directories
    "Delete" button now deletes any directory inside the selection,
    "selection" refers to the newly added "ImGuiSelectionBasicStorage"

--- src/files/Directories.h
+++ src/files/Directories.h
@@ -1,7 +1,15 @@
 #pragma once
 
+#include <imgui.h>
+
 #include <vector>
 #include <filesystem>
+
+struct DirectoryItem
+{
+    ImGuiID ID;
+    std::string path;
+};
 
 class Directories
 {
@@ -9,11 +17,12 @@ public:
     using Path = std::filesystem::path; // Creates a "type alias" for std::filesystem::path (nothing changes, just easier for me to read)
     const std::vector<Path>& getDirectories() const { return m_directories; } // Essentially returns a read-only REFERENCE to the directories array
 
-    void promptDirectorySelect(); // Long/confusing name, might change
+    void promptDirectorySelect(); // Currently doesn't work (Long/confusing name, might change)
     void addDirectory(const std::string& path);
     void removeDirectory(const std::string& path);
+    void removeDirectories(const std::vector<std::string>& paths);
     void clearDirectories();
-    void scanDirectories();
+    // void scanDirectories();
 private:
     // Vector array containing all directories
     std::vector<Path> m_directories;
--- src/ui/contents/DirectoriesUI.cpp
+++ src/ui/contents/DirectoriesUI.cpp
@@ -13,77 +13,115 @@ static const std::string TEST_DIR = "/home/UserHere/Music/";
 
 void DirectoriesUI::render()
 {
-  // Top buttons
-  ImGui::Button("Rescan Directories");
+    // These statics are used for the ImGui selection
+    static ImGuiSelectionBasicStorage selection;
+    static std::vector<DirectoryItem> items;
 
-  ImGui::SameLine();
+    // Get directories
+    const std::vector<std::filesystem::path>& directoryList = m_directories.getDirectories();
 
-  // Push green button style
-  ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.1f, 0.5f, 0.1f, 1.0f));
-  ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(0.1f,0.75f,0.1f,1.0f));
-  ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(0.1f,0.65f,0.1f,1.0f));
+    // Top buttons
+    ImGui::Button("Rescan Directories");
+
+    ImGui::SameLine();
+
+    // Push green button style
+    ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.1f, 0.5f, 0.1f, 1.0f));
+    ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(0.1f,0.75f,0.1f,1.0f));
+    ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(0.1f,0.65f,0.1f,1.0f));
 
 
-  if (ImGui::Button("Create"))
-  {
-    // For now temporarily, use the test directory
-    m_directories.addDirectory(TEST_DIR);
-  }
-  ImGui::PopStyleColor(3);
+    if (ImGui::Button("Create"))
+    {
+        // For now temporarily, use the test directory
+        m_directories.addDirectory(TEST_DIR);
+    }
+    ImGui::PopStyleColor(3);
 
-  ImGui::SameLine();
+    ImGui::SameLine();
 
-  // Push red button style
-  ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.5f, 0.1f, 0.1f, 1.0f));
-  ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(0.75f,0.1f,0.1f,1.0f));
-  ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(0.65f,0.1f,0.1f,1.0f));
+    // Push red button style
+    ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.5f, 0.1f, 0.1f, 1.0f));
+    ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(0.75f,0.1f,0.1f,1.0f));
+    ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(0.65f,0.1f,0.1f,1.0f));
 
-  if (ImGui::Button("Remove"))
-  {
-    // TODO: Check the currently selected button and remove it
-    // For now temporarily, use the test directory
-    m_directories.removeDirectory(TEST_DIR);
-  }
+    if (ImGui::Button("Remove"))
+    {
+        for (const auto& item : items)
+        {
+            if (selection.Contains(item.ID))
+            {
+                m_directories.removeDirectory(item.path);
+            }
+        }
+        items.clear(); // Clear items so they repopulate next frame
+    }
 
-  ImGui::SameLine();
+    ImGui::SameLine();
 
-  // manageDirWidth gets the buttons "width" before it is created
-  float manageDirWidth = ImGui::CalcTextSize("Manage directories here").x;
+    // manageDirWidth gets the buttons "width" before it is created
+    float manageDirWidth = ImGui::CalcTextSize("Manage directories here").x;
 
-  // clearButtonWidth gets the buttons "width" before it is created
-  float clearButtonWidth = ImGui::CalcTextSize("Clear all Directories").x + ImGui::GetStyle().FramePadding.x * 2; 
+    // clearButtonWidth gets the buttons "width" before it is created
+    float clearButtonWidth = ImGui::CalcTextSize("Clear all Directories").x + ImGui::GetStyle().FramePadding.x * 2; 
 
-  // The code below calculates where to place the "Manage directories here" text (i love working on random useless things)
-  float clearButtonStart = ImGui::GetWindowWidth() - clearButtonWidth - WINDOW_PADDING;
+    // The code below calculates where to place the "Manage directories here" text (i love working on random useless things)
+    float clearButtonStart = ImGui::GetWindowWidth() - clearButtonWidth - WINDOW_PADDING;
 
-  // CursorPosX stays the same even after creating the "Remove" button, so we can do this
-  float centerPos = (ImGui::GetCursorPosX() + clearButtonStart) / 2.0f - (manageDirWidth / 2.0f);
+    // CursorPosX stays the same even after creating the "Remove" button, so we can do this
+    float centerPos = (ImGui::GetCursorPosX() + clearButtonStart) / 2.0f - (manageDirWidth / 2.0f);
 
-  ImGui::SetCursorPosX(centerPos);
-  ImGui::Text("Manage directories here");
+    ImGui::SetCursorPosX(centerPos);
+    ImGui::Text("Manage directories here");
 
-  ImGui::SameLine();
+    ImGui::SameLine();
 
-  ImGui::SetCursorPosX(ImGui::GetWindowWidth() - clearButtonWidth - WINDOW_PADDING);
+    ImGui::SetCursorPosX(ImGui::GetWindowWidth() - clearButtonWidth - WINDOW_PADDING);
 
-  if (ImGui::Button("Clear all Directories"))
-  {
-    m_directories.clearDirectories();
-  }
-  
-  ImGui::PopStyleColor(3);
+    if (ImGui::Button("Clear all Directories"))
+    {
+        m_directories.clearDirectories();
+    }
 
-  // Tables
-  // TODO: Potentially add functionality for multi-select using ImGui::BeginMultiSelect()
-  // Basic documentation for multi-selecting can be found here: https://github.com/ocornut/imgui/wiki/Multi-Select
-  if (ImGui::BeginTable("DirectoriesTable", 1, ImGuiTableFlags_RowBg))
-  {
-      for (const auto& dir : m_directories.getDirectories())
-      {
-        ImGui::TableNextRow();
-        ImGui::TableSetColumnIndex(0);
-        ImGui::Selectable(dir.c_str());
-      }
-      ImGui::EndTable();
-  }
+    ImGui::PopStyleColor(3);
+
+    // Tables
+    // Basic documentation for multi-selecting can be found here: https://github.com/ocornut/imgui/wiki/Multi-Select
+
+    // Gives a unique ID to each directory in the list
+    if (items.empty())
+    {
+        ImGuiID id = 0;
+        for (const auto& dir : directoryList)
+        {
+            items.push_back({id++, dir.string()});
+        }
+    }
+
+    selection.UserData = (void*)&items;
+    selection.AdapterIndexToStorageId = [](ImGuiSelectionBasicStorage* self, int index)
+    {
+        auto* item_array = static_cast<std::vector<DirectoryItem>*>(self->UserData);
+        return (*item_array)[index].ID;
+    };
+
+    ImGuiMultiSelectIO* multiSelectIO = ImGui::BeginMultiSelect(ImGuiMultiSelectFlags_None, selection.Size, items.size());
+    selection.ApplyRequests(multiSelectIO);
+    
+    if (ImGui::BeginTable("DirectoriesTable", 1, ImGuiTableFlags_RowBg))
+    {
+        for (int index = 0; index < items.size(); index++)
+        {
+            bool item_is_selected = selection.Contains(items[index].ID);
+            ImGui::SetNextItemSelectionUserData(index);
+            
+            ImGui::TableNextRow();
+            ImGui::TableSetColumnIndex(0);
+            ImGui::Selectable(items[index].path.c_str(), item_is_selected);
+        }
+        ImGui::EndTable();
+    }
+
+    multiSelectIO = ImGui::EndMultiSelect();
+    selection.ApplyRequests(multiSelectIO);
 }
