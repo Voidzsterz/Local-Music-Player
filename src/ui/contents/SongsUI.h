89b7f1cc3a0f79550dc39891f00b4d5ef715823e
Voidzsterz <fabian.isib@gmail.com>
2026-06-13 19:28:38 +0100

    Scrapped tree nodes entirely
    Decided to remove the "tree idea" entirely, I feel it'd be confusing and
    hard to navigate especially for just a music player.
    
    Will try and go for a "file explorer" approach instead where you can
    open folders and they'll display their contents on a new page.

--- src/ui/contents/SongsUI.h
+++ src/ui/contents/SongsUI.h
@@ -2,15 +2,7 @@
 
 #include "../../files/Directories.h"
 
-
-
 class Renderer;
-
-struct SongItem
-{
-    ImGuiID ID;
-    std::string path;
-};
 
 class SongsUI
 {
@@ -21,14 +13,5 @@ private:
     Renderer& m_renderer;
     Directories& m_directories;
 
-    ImGuiSelectionBasicStorage m_selection;
-    std::vector<SongItem> m_songItems;
-
     void renderTopbar();
-    void renderTree(const PathNode& node, int& songIndex);
-
-    void collectSongs(PathNode& node, int& index);
-    
-    // Caching allows huge performance increases (The program was crashing when the songs tab opened originally)
-    std::vector<PathNode> m_cachedTrees;
 };
